f264bc1bfda6f63e4667a12263c073b0ee9424cd
IRCraziestTaxi <ircraziesttaxi@gmail.com>
2019-11-20 08:50:38 -0600

    added workaround and linked TS issue this repro was made for.

--- src/_common/interfaces/return-type-query.interface.ts
+++ src/_common/interfaces/return-type-query.interface.ts
@@ -1,3 +1,6 @@
 import { IQuery } from "@nestjs/cqrs";
 
-export interface IReturnTypeQuery<TResult> extends IQuery { }
+export interface IReturnTypeQuery<TResult> extends IQuery {
+    // https://github.com/microsoft/TypeScript/issues/35217
+    __type: TResult;
+}
--- src/user/queries/get-users/get-users.query.ts
+++ src/user/queries/get-users/get-users.query.ts
@@ -1,9 +1,7 @@
 import { IReturnTypeQuery } from "src/_common/interfaces/return-type-query.interface";
 import { User } from "../../../user/user.entity";
 
-// This produces no error although according to IReturnTypeQueryHandler,
-// the type argument for this query should match TResult of GetUsersHandler,
-// which is User[] rather than User.
-// Omitting the implementation of this interface altogether (no "implements")
-// also produces the same result (no error in the handler and "unknown" in the controller).
-export class GetUsersQuery implements IReturnTypeQuery<User> { }
+export class GetUsersQuery implements IReturnTypeQuery<User[]> {
+    // https://github.com/microsoft/TypeScript/issues/35217
+    public __type: User[];
+}
